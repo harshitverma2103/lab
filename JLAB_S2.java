1664d1bf4e9ed5f59c21f5d6c5fc9b2f3797c9d7
harshitverma2103 <harshitverma2103@gmail.com>
2022-09-01 00:37:25 +0530

    add JLAB_S2 with method T1

---
+++ JLAB_S2.java
@@ -0,0 +1,9 @@
+public class JLAB_S2 {
+    public static void main(String[] args) {
+        System.out.println(T1(2));
+    }
+
+    public static int T1(int n) {
+        return n * n + n * n * n;
+    }
+}
