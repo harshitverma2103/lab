121ef765c0ed1bd9fbb5eeab3b2ce4fe7d571bc1
harshitverma2103 <harshitverma2103@gmail.com>
2022-09-01 15:52:27 +0530

    add more values

--- JLAB_S4.java
+++ JLAB_S4.java
@@ -1,14 +1,18 @@
 public class JLAB_S4 {
     public static void main(String[] args) {
-        T1(15);
+        T1_1(15);
+        T1_1(7);
         int[] numArr = { 21, 4, 12, 32, 33 };
-        T2(numArr);
+        T1_2(numArr);
         int[] numberArr = { 10, 0, 3, 4, 5 };
-        T3(numberArr);
-        T4( 94);
+        T2(numberArr);
+        T3_1(94);
+        T3_1(36);
+        T3_1(40);
+        T3_1(80);
     }
 
-    public static void T1(int n) {
+    public static void T1_1(int n) {
         if (n % 3 == 0) {
             System.out.println(n + ":Yes");
         } else {
@@ -16,13 +20,13 @@ public class JLAB_S4 {
         }
     }
 
-    public static void T2(int[] numArr) {
+    public static void T1_2(int[] numArr) {
         for (int i = 0; i < numArr.length; i++) {
-            T1( numArr[i]);  
+            T1_1(numArr[i]);  
         }
     }
 
-    public static void T3(int[] numberArr) {
+    public static void T2(int[] numberArr) {
         int size = 0;
         for (int i = 0; i < numberArr.length; i++) {
             int n = numberArr[i];
@@ -33,7 +37,7 @@ public class JLAB_S4 {
         System.out.println(size);
     }
 
-    public static void T4(int marks) {
+    public static void T3_1(int marks) {
         if (marks >= 90) {
             System.out.println("grade A");
         } else if (marks >= 75) {
