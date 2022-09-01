d4e9fe41f35173ae9b30188ee88deb0a79c53d90
harshitverma2103 <harshitverma2103@gmail.com>
2022-09-01 13:28:46 +0530

    code refactor

--- JLAB_S3.java
+++ JLAB_S3.java
@@ -1,8 +1,8 @@
 public class JLAB_S3 {
     public static void main(String[] args) {
         T1(4);
-        int[] numarr = { 10, 9, 12, 5, 7 };
-        T2(numarr);
+        int[] numArr = { 10, 9, 12, 5, 7 };
+        T2(numArr);
         T3(4);
         int[] numberarr = { 5, 13, 15, 20, 36 };
         T4(numberarr);
@@ -16,14 +16,9 @@ public class JLAB_S3 {
         }
     }
 
-    public static void T2(int[] numarr) {
-        for (int i = 0; i < numarr.length; i++) {
-            int n = numarr[i];
-            if (n % 2 == 0) {
-                System.out.println(n + ":" + "even");
-            } else {
-                System.out.println(n + ":" + "odd");
-            }
+    public static void T2(int[] numArr) {
+        for (int i = 0; i < numArr.length; i++) {
+            T1(numArr[i]);
         }
     }
 
@@ -41,16 +36,7 @@ public class JLAB_S3 {
 
     public static void T4(int[] numberarr) {
         for (int i = 0; i < numberarr.length; i++) {
-            int n = numberarr[i];
-            if (n % 2 == 0) {
-                System.out.println("pepsi");
-            } else if (n % 3 == 0) {
-                System.out.println("Coke");
-            } else if (n % 2 == 0 || n % 3 == 0) {
-                System.out.println("pepsicoke");
-            } else {
-                System.out.println("paperBoat");
-            }
+            T3(numberarr[i]);
         }
     }
 }
