e4d41bd88789cea391793916d2abed63c7c9b97d
harshitverma2103 <harshitverma2103@gmail.com>
2022-09-06 00:04:42 +0530

    fix variable name

--- JLAB_Q1.java
+++ JLAB_Q1.java
@@ -1,6 +1,6 @@
 public class JLAB_Q1 {
     public static void main(String[] args) {
-        T1("ZZZ");
+        T1("zzz");
         T1("zackz");
         T1("ratcatmat");
         T2("tRiSeCt100");
@@ -9,28 +9,28 @@ public class JLAB_Q1 {
     }
 
     public static void T1(String word) {
-        int count = 0;
+        int charCount = 0;
         for (int index = 0; index < word.length(); index++) {
             char ch = word.charAt(index);
-            if (ch == 'Z') {
-                count = count + 1;
+            if (ch == 'z') {
+                charCount = charCount + 1;
             }
         }
-        System.out.println(count);
+        System.out.println(charCount);
     }
 
     public static void T2(String word) {
-        int count1 = 0;
-        int count2 = 0;
+        int lowerCharCount = 0;
+        int upperCharCount = 0;
         for (int index = 0; index < word.length(); index++) {
             char ch = word.charAt(index);
             if (ch >= 'a' && ch <= 'z') {
-                count1 = count1 + 1;
+                lowerCharCount = lowerCharCount + 1;
             } else if (ch >= 'A' && ch <= 'Z') {
-                count2 = count2 + 1;
+                upperCharCount = upperCharCount + 1;
             }
         }
-        System.out.println("Lower =" + count2);
-        System.out.println("Upper =" + count1);
+        System.out.println("Upper =" + upperCharCount);
+        System.out.println("Lower =" + lowerCharCount);
     }
 }
