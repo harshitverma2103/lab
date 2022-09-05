6d62e57fcf0cfc586c1149a56aef3103e17237eb
harshitverma2103 <harshitverma2103@gmail.com>
2022-09-05 16:11:51 +0530

    add more values

--- JLAB_S5.java
+++ JLAB_S5.java
@@ -1,12 +1,15 @@
 public class JLAB_S5 {
     public static void main(String[] args) {
-        T1("Trisect");
+        T1_1("Trisect");
+        T1_1("HELLO");
+        T1_1("Za");
         String[] arr = { "TEST", "HeeL", "car" };
-        T2(arr);
-        T3("trisect", 1);
+        T1_2(arr);
+        T2("trisect", 1);
+        T2("hello", 0);
     }
 
-    public static void T1(String str) {
+    public static void T1_1(String str) {
         int count = 0;
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
@@ -17,20 +20,31 @@ public class JLAB_S5 {
         System.out.println("LowerCount-" + count);
     }
 
-    public static void T2(String[] arr) {
+    public static void T1_2(String[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            T1(arr[i]);
+            T1_1(arr[i]);
         }
     }
 
-    public static void T3(String word, int n) {
+    public static void T2(String word, int n) {
         String qstr = "";
         for (int i = 0; i < word.length(); i++) {
             char ch = word.charAt(i);
-            if (n == 1 && i % 2 == 0) {
-                qstr = qstr + "#" + ch;
+            if (n == 1) {
+                if (i % 2 == 0) {
+                    qstr = qstr + ch;
+                } else {
+                    qstr = qstr + "#";
+                }
+            }    
+            else  {
+                if(n == 0) {
+                    qstr = qstr + "#";
+                } else if (i % 2 != 0) {
+                   qstr = qstr + ch;
+                 }
             }
         }
-        System.out.println(qstr);
+        System.out.println(qstr);  
     }
 }
