af33f194d421b9d6eff3791a8e6ec8b2b0467731
harshitverma2103 <harshitverma2103@gmail.com>
2022-09-30 23:39:41 +0530

    add method T4,T10,T11,T12 and T13

--- JLAB_M6.java
+++ JLAB_M6.java
@@ -3,9 +3,14 @@ public class JLAB_M6 {
         T1(5);
         T2(5);
         T3(5);
+        T4(4);
         T5(5);
         T6(5);
         T7(5);
+        T8(5);
+        T9(5);
+        T10(5);
+        T11(5);
     }
 
     public static void T1(int number) {
@@ -39,6 +44,25 @@ public class JLAB_M6 {
         }
     }
 
+    public static void T4(int number) {
+        String pattern = "";
+        for (int i = 1; i <= number; i++) {
+            pattern += i;
+            System.out.println(pattern);
+        }
+        if (pattern.length() == number) {
+            int num = number;
+            for (int i = 1; i <= number; i++) {
+                String pattern1 = "";
+                for (int j = 1; j <= num; j++) {
+                    pattern1 += j;
+                }
+                num--;
+                System.out.println(pattern1);
+            }
+        }
+    }
+
     public static void T5(int number) {
         String result = "";
         int num = number;
@@ -51,7 +75,7 @@ public class JLAB_M6 {
 
     public static void T6(int number) {
         String pattern = "";
-        for(int index = 1; index <= number; index++) {
+        for (int index = 1; index <= number; index++) {
             pattern = index + pattern;
             System.out.println(pattern);
         }
@@ -63,6 +87,56 @@ public class JLAB_M6 {
             for (int j = 1; j <= i; j++) {
                 pattern += i;
             }
+            System.out.println(pattern);
+        }
+    }
+
+    public static void T8(int number) {
+        String pattern = "";
+        for (int index = 1; index <= number; index++) {
+            if (index == 1) {
+                pattern = pattern + index;
+                System.out.println(pattern);
+
+            } else {
+                pattern = index + pattern + index;
+                System.out.println(pattern);
+            }
+        }
+    }
+
+    public static void T9(int number) {
+        int num = 1;
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void T10(int number) {
+        int num = number;
+        for (int i = 1; i <= number; i++) {
+            String pattern = "";
+            for (int j = 1; j <= num; j++) {
+                pattern += j;
+            }
+            num--;
+            System.out.println(pattern);
+        }
+    }
+
+    public static void T11(int number) {
+        int tempNum = 1;
+        int num = number;
+        for (int i = 1; i <= number; i++) {
+            String pattern = "";
+            for (int j = num; j >= tempNum; j--) {
+                pattern += j;
+            }
+            tempNum++;
             System.out.println(pattern);
         }
     }
